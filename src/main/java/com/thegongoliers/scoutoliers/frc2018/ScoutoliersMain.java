3dcc82585c96e61c69870163ec34dddf2b3fa8f5
Nicholas Bottone <nicholasbottone@gmail.com>
2018-01-30 17:01:49 -0500

    Swithed to Eclipse-based Maven Project
    Switched from IntelliJ IDEA IDE to Eclipse IDE, switched from a
    Gradel-based project to a Maven-based project, and finally added several
    new and potentially useful dependencies to the pom.xml file.  These
    dependencies could be used for building HTML webpages or sending
    information to and from the client.

--- src/main/java/com/thegongoliers/scoutoliers/frc2018/ScoutoliersMain.java
+++ src/main/java/com/thegongoliers/scoutoliers/frc2018/ScoutoliersMain.java
@@ -3,15 +3,28 @@ package com.thegongoliers.scoutoliers.frc2018;
 import static spark.Spark.*;
 
 /**
+ * This is the backend server to a scouting web app made for FIRST Power Up
+ * 2018. We are The Gongoliers, FRC Team 5112. We are based in North Scituate,
+ * Rhode Island at Ponaganset High School. Developed by Nicholas Bottone, with
+ * special thanks to Joseph Mazzone and Kyle Corry.
+ * 
+ * Visit our GitHub Repository:
+ * https://github.com/Gongoliers/ScoutoliersBackend-2018
+ * 
  * @author Nicholas Bottone
- * @since 1.0-SNAPSHOT (1/30/2018)
+ * @version 0.0.1-SNAPSHOT
+ * @since 0.0.1-SNAPSHOT (01/30/2018)
  */
 public class ScoutoliersMain {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        get("/hello", (req, res) -> "Hello World");
+		/*
+		 * Hosts an HTML page with the text "Hello World" at the address
+		 * http://localhost:4567/hello
+		 */
+		get("/hello", (req, res) -> "Hello World");
 
-    }
+	}
 
 }
