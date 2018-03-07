2006b5fe37594ee2ffada4a9f25d8a735345a7bb
Nicholas Bottone <NicholasBottone@gmail.com>
2018-03-07 02:58:59 -0500

    Setup Home Screen Layout

--- ScoutoliersFrontend-2018/src/com/thegongoliers/scoutoliers/powerup2018/frontend/HomeScreen.java
+++ ScoutoliersFrontend-2018/src/com/thegongoliers/scoutoliers/powerup2018/frontend/HomeScreen.java
@@ -1,5 +1,11 @@
 package com.thegongoliers.scoutoliers.powerup2018.frontend;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,16 +24,62 @@ public class HomeScreen {
 	public JLabel l1;
 	public JLabel l2;
 	public JLabel l3;
-	public JLabel l4;
 	
 	public JButton b1;
 	public JButton b2;
 	public JButton b3;
-	public JButton b4;
 	
 	public HomeScreen() {
 		
-		// TODO
+		frame = new JFrame("Scoutoliers - FIRST Power Up");
+		
+		im = new JLabel(FrontendMain.ws.logo);
+		l1 = new JLabel("Scoutoliers 2018");
+		l2 = new JLabel("FIRST Power Up");
+		l3 = new JLabel("Please select your desired action.");
+		
+		b1 = new JButton("PRE-SCOUTING");
+		b2 = new JButton("MATCH SCOUTING");
+		b3 = new JButton("VIEW RANKINGS");
+		
+		l1.setFont(new Font("", Font.BOLD, 48));
+		l2.setFont(new Font("", Font.BOLD, 24));
+		l3.setFont(new Font("", Font.ITALIC, 16));
+		
+		b1.setFont(new Font("", Font.PLAIN, 20));
+		b2.setFont(new Font("", Font.PLAIN, 20));
+		b3.setFont(new Font("", Font.PLAIN, 20));
+		
+		b1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				frame.dispose();
+				// TODO: Go to Pre-Scouting screen
+			}
+		});
+		
+		b2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				frame.dispose();
+				// TODO: Go to Match Scouting screen
+			}
+		});
+		
+		b3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				frame.dispose();
+				// TODO: Go to team ranking screen
+			}
+		});
+		
+		frame.getContentPane().setBackground(Color.ORANGE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setIconImage(FrontendMain.ws.icon.getImage());
+		frame.setLayout(new FlowLayout());
+		frame.setSize(400, 650);
+		frame.setVisible(true);
 		
 	}
 	
